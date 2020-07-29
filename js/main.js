819f158a03828b3c2e58f49c3d647d622700fe3a
Shayan199177 <sh.salimzadeh@gmail.com>
2020-07-29 21:22:24 +0430

    smart-scroll & hide navBar

--- js/main.js
+++ js/main.js
@@ -13,6 +13,23 @@ $(document).ready(function () {
     });
 });
 
+// add padding top to show content behind navbar
+$('body').css('padding-top', $('.navbar').outerHeight(85) + 'px')
+
+// detect scroll top or down
+if ($('.smart-scroll').length > 0) { // check if element exists
+    var last_scroll_top = 0;
+    $(window).on('scroll', function() {
+        scroll_top = $(this).scrollTop();
+        if(scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
+    });
+}
 
 jQuery('.slider-1').owlCarousel({
     rtl: true,
