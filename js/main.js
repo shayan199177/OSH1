1a85e0781d54393e5d407a8f376cc60f07921dd8
Shayan199177 <sh.salimzadeh@gmail.com>
2020-08-13 21:22:04 +0430

    fix smart scroll

--- js/main.js
+++ js/main.js
@@ -31,6 +31,27 @@ if ($('.smart-scroll').length > 0) { // check if element exists
     });
 }
 
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 jQuery('.slider-1').owlCarousel({
     rtl: true,
     loop: false,
